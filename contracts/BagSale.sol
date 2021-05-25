a0b3aab107a703898d2edf8b50d37b5912cc022f
chichi <noreply@github.com>
2021-05-25 16:22:07 +0300

    BagSale prod version

--- contracts/BagSale.sol
+++ contracts/BagSale.sol
@@ -1,24 +1,20 @@
 // SPDX-License-Identifier: MIT
-// Degen Farm. Collectible NFT game
+// Degen'$ Farm: Collectible NFT game (https://degens.farm)
 pragma solidity ^0.7.4;
 
 import "OpenZeppelin/openzeppelin-contracts@3.4.0/contracts/access/Ownable.sol";
 import "./BagsERC20.sol";
 
 contract BagSale is Ownable {
-    using SafeMath for uint256; 
+    using SafeMath for uint256;
 
-    //TODO!!!!!!!!!!UNCOMMENT THIS BEFORE PRODUCTION and set value
-    //uint256 constant SALE_AFTER = 0; //!!!!! Set in production!!!!
     Bags immutable bagsContract;
-    
-    //TODO!!!!!!!!!! REMOVE THIS BEFORE  PRODUCTION
-    uint256 public SALE_AFTER = 0;
+
     uint256 public bagPrice = 1e17;
     uint256 public weiRaised;
 
     event BagBought(address indexed _buyer, uint256 _boughtPrice, uint256 amount);
- 
+
     /**
      * @dev Set some initial params for sale.
      *
@@ -31,14 +27,12 @@ contract BagSale is Ownable {
     }
 
     /**
-     * @dev Call this Function for buy bag for ether 
+     * @dev Call this Function for buy bag for ether
      *
      */
     function buyBag() external payable {
-        require(block.timestamp > SALE_AFTER, "Can't by before SALE_AFTER");
         require(msg.value >= bagPrice, "Need more ether!");
         uint256 mintAmount = msg.value.mul(10**bagsContract.decimals()).div(bagPrice);
-        // require(mintAmount <= 10e18, "Cant't buy more than 10 per tx!");
         bagsContract.mint(msg.sender, mintAmount);
         weiRaised = weiRaised.add(msg.value);
         emit BagBought(msg.sender, bagPrice, mintAmount);
@@ -50,18 +44,6 @@ contract BagSale is Ownable {
     function withdraw() external onlyOwner {
         msg.sender.transfer(address(this).balance);
     }
-
-    
-    //TODO: Remove before PRODUCTION  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    function setSaleStart(uint256 _start_date) external onlyOwner {
-        SALE_AFTER = _start_date;
-    }
-
-    //TODO: Remove before PRODUCTION  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    function setBagPrice(uint256 _bagPrice) external onlyOwner {
-        bagPrice = _bagPrice;
-    }
-
 
     /**
      * @dev Returns amount of bags that available for sale yet
@@ -78,7 +60,7 @@ contract BagSale is Ownable {
     }
 
     /**
-     * @dev Function returns tuple with next price  according to the bonding 
+     * @dev Function returns tuple with next price  according to the bonding
      * curve (1) and left amount of BAGs token (2)
      *
      * @return (uint256, uint256)
