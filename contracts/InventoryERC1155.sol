21e6fe29efbe473b4668d10d2bfcd9a7c7275e08
chichi <noreply@github.com>
2021-06-09 03:12:10 +0300

    Prod dung swap prices

--- contracts/InventoryERC1155.sol
+++ contracts/InventoryERC1155.sol
@@ -1,68 +1,95 @@
 // SPDX-License-Identifier: MIT
+// Degen'$ Farm: Collectible NFT game (https://degens.farm)
 pragma solidity ^0.7.4;
 
 import "OpenZeppelin/openzeppelin-contracts@3.4.0/contracts/token/ERC1155/ERC1155.sol";
 import "OpenZeppelin/openzeppelin-contracts@3.4.0/contracts/access/Ownable.sol";
 import "OpenZeppelin/openzeppelin-contracts@3.4.0/contracts/utils/Strings.sol";
-//For initial change inventary for DUNG
+//For initial change inventory for DUNG
 import "OpenZeppelin/openzeppelin-contracts@3.4.0/contracts/token/ERC20/IERC20.sol";
 
-//v0.0.1
-contract InventaryERC1155 is  ERC1155 {
+interface IDungBurn is IERC20 {
+    function burn(
+        uint256 amount 
+    ) external;
+}
+
+contract InventoryERC1155 is ERC1155, Ownable {
     using Strings for uint256;
     using SafeMath for uint256;
 
-    enum   InventaryType {Pipe, Hat, Carrot, Shovel, Rake, Pitchfork}
-    struct Inventary {
-        InventaryType itype;
+    uint256 constant public TOOL_TYPE_COUNT = 6;
+    enum   InventoryType {Hat, Pipe, Moonshine, Shovel, Rake, Pitchfork}
+
+    struct Inventory {
+        InventoryType itype;
         uint256       priceDung;
-        uint256       incProbability;
+        uint16        incProbability;
     }
-
-    string constant BASE_METADATA_URL= "https://nft.iber.group/degenfarm/V1/inventary/";
-
+    
+    string constant BASE_METADATA_URL = "https://degens.farm/meta/inventory/";
+    
     address public dungERC20;
     //Token id to Properties
-    mapping(uint8 => Inventary) public inventaryProperties;
+    mapping(uint8 => Inventory) public inventoryProperties;
 
+    
+    /**
+     * @dev we use uri_ here for Compatibility with
+     * https://eips.ethereum.org/EIPS/eip-1155#metadata
+     * 
+     */
+    constructor (string memory uri_, address _dungToken)
+        ERC1155(uri_)
+    {
+        dungERC20 = _dungToken;
+        register(InventoryType.Hat,       25e18 ether, 5, 20);
+        register(InventoryType.Pipe,      15e18 ether, 10, 10);
+        register(InventoryType.Moonshine, 7e18 ether,  25, 8);
+        register(InventoryType.Shovel,    5e18 ether,  50, 5);
+        register(InventoryType.Rake,      2500e15 ether, 30, 4);
+        register(InventoryType.Pitchfork, 1e18 ether,  100, 2);
+    }
 
-    constructor (string memory uri_) ERC1155(uri_) {
-        _mint(address(this), 0, 5, bytes('0')); //Hat
-        inventaryProperties[0] = Inventary(InventaryType.Pipe,      200e18, 20);
-        _mint(address(this), 1, 3, bytes('0'));
-        inventaryProperties[1] = Inventary(InventaryType.Hat,       180e18, 15);
-        _mint(address(this), 2, 5, bytes('0'));
-        inventaryProperties[2] = Inventary(InventaryType.Carrot,    150e18, 12);
-        _mint(address(this), 3, 5, bytes('0'));
-        inventaryProperties[3] = Inventary(InventaryType.Shovel,    120e18, 10);
-        _mint(address(this), 4, 5, bytes('0'));
-        inventaryProperties[4] = Inventary(InventaryType.Rake,      120e18, 10);
-        _mint(address(this), 5, 3, bytes('0'));
-        inventaryProperties[5] = Inventary(InventaryType.Pitchfork, 120e18, 10);
+    function register(InventoryType _type, uint price, uint count, uint16 boost) internal {
+        _mint(address(this), uint8(_type), count, bytes('0'));
+        inventoryProperties[uint8(_type)] = Inventory(_type, price, boost);
     }
 
     function dungSwap(uint8 _item, uint256 _amount) external {
-        require(IERC20(dungERC20).balanceOf(msg.sender) >=
-            _amount.mul(inventaryProperties[_item].priceDung),
+        require(_amount != 0, "Cant swap zero dung");
+        require(IERC20(dungERC20).balanceOf(msg.sender) >= 
+            _amount.mul(inventoryProperties[_item].priceDung), 
             "Insufficient DUNG!"
         );
         IERC20(dungERC20).transferFrom(
-            msg.sender,
-            address(this),
-            _amount.mul(inventaryProperties[_item].priceDung)
+            msg.sender, 
+            address(this), 
+            _amount.mul(inventoryProperties[_item].priceDung)
         );
-        safeTransferFrom(address(this), msg.sender, _item, _amount, bytes('0'));
+        IDungBurn(dungERC20).burn(_amount.mul(inventoryProperties[_item].priceDung));
+
+        this.safeTransferFrom(address(this), msg.sender, _item, _amount, bytes('0'));
     }
 
-
-    function uri(uint256 _id) external view override returns (string memory) {
+    /*
+     * @dev this is not full standard bot common use of 1155 implementation
+     */
+    function uri2(uint256 _id) external view  returns (string memory) {
         return string(abi.encodePacked(BASE_METADATA_URL, _id.toString()));
     }
 
-    function getToolBoost(uint8 _item) external view returns (uint256) {
-        return inventaryProperties[_item].incProbability;
+    function getToolBoost(uint8 _item) external view returns (uint16) {
+        return inventoryProperties[_item].incProbability;
+    }
+
+    function getMarketInfo() external view returns (uint[] memory result) {
+        result = new uint[](TOOL_TYPE_COUNT*3);
+        for (uint8 i = 0; i < TOOL_TYPE_COUNT; i++) {
+            result[i*3 + 0] = inventoryProperties[i].priceDung;
+            result[i*3 + 1] = inventoryProperties[i].incProbability;
+            result[i*3 + 2] = balanceOf(address(this), i);
+        }
     }
 
 }
-
-
